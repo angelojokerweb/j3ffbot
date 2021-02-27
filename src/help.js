f0380773fa242a0eacddc8d4a8e35730f46286f2
angelojokerweb <79103848+angelojokerweb@users.noreply.github.com>
2021-02-27 00:20:23 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,86 @@
+const help = (prefix) => {
+	return `>
+CRIADOR: ANGELO\n WPP: wa.me/+5564992606313\n
+Comando do J3FFBOT?* <
+	*Apenas Se Divirta Usando ele ??*
+comando : *${prefix}sticker* or *${prefix}stiker*
+desc : converte imagem/gif/video para sticker
+uso: responder imagem / gif / vÃ­deo ou enviar imagem / gif / vÃ­deo com legenda\n
+comando : *${prefix}sticker nobg* or *${prefix}stiker nobg*
+desc : converter imagem em adesivo removendo o fundo
+uso : reply image, or send image with caption\n
+comando : *${prefix}imagem*
+desc : converte sticker para imagem
+uso : reply sticker\n
+comando : *${prefix}tsticker* or *${prefix}tstiker*
+desc : converte texto para sticker
+uso : *${prefix}tsticker text in here*\n
+> *?Meme Commandos?* < [OFF]
+comando : *${prefix}meme (off)*
+desc : imagens de memes aleatÃ³rios [english]
+uso : just send the command\n
+comando : *${prefix}memeindo (off)*
+desc : imagens de memes aleatÃ³rios [indo]
+uso : just send the command\n
+> *,?Outros Comandos?* <
+comando : *${prefix}gtts*
+desc : converte texto para audio
+uso : *${prefix}gtts [cc] [text]*\nexample : *${prefix}gtts ja On2-chan*\n
+comando : *${prefix}loli*
+desc : imagens aleatÃ³rias de loli
+uso : just send the command\n
+comando : *${prefix}nsfwloli*
+desc : imagens nsfw aleatÃ³rias de loli
+uso : just send the command\n
+comando : *${prefix}url2img*
+desc : tirar screenshots da web
+uso : *${prefix}url2img [tipe] [url]*\n
+comando : *${prefix}simi*
+desc : your message will be replied to by simi
+uso : *${prefix}simi yourmessage*\n
+comando : *${prefix}ocr*
+desc : take the text in the picture
+uso : reply image, or send image with caption\n
+comando : *${prefix}wait*
+desc : pesquisar anime com imagem [Que anime Ã© isso / aquilo]
+uso : reply image, or send image with caption\n
+comando : *${prefix}setprefix*
+desc : escolhe um novo prefixo
+uso : *${prefix}setprefix [text|optional]*\nexample : *${prefix}setprefix ?*
+note : Este comando sÃ³ pode ser usado pelo proprietÃ¡rio do bot\n
+> *?Grupo Comandos?* < ---
+comando : *${prefix}add*
+desc : adiciona um membro no grupo
+uso : *${prefix}add 62813xxxxx*\n
+note : s¨® pode ser usado quando o bot se torna admin, e quem envia o comando Ã© admin!\n
+comando : *${prefix}ban*
+desc : expulsa membros do grupo 
+uso : *${prefix}ban @tagmember*\n
+note : s¨® pode ser usado quando o bot se torna admin, e quem envia o comando Ã© admin!\n
+comando : *${prefix}promover*
+desc : Faz ser ademe do grupo 
+uso : *${prefix}promover @tagmember*\n
+note : s¨® pode ser usado quando o bot se torna admin, e quem envia o comando Ã© admin!\n
+comando : *${prefix}rebaixar*
+desc : tira os poderes do ademiro 
+uso : *${prefix}rebaixar @tagmember*\n
+note : s¨® pode ser usado quando o bot se torna admin, e quem envia o comando Ã© admin!\n
+comando : *${prefix}linkgroup*
+desc : pega o link desse Grupo
+uso : just send the command
+note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
+comando : *${prefix}botsair*
+desc : faz o bot sair do grupo (faz isso n?o :( )
+uso : just send the command
+note : S¨® pode ser usado por administradores de grupo e proprietÃ¡rio do bot\n
+comando : *${prefix}tagall*
+desc : Chama geral do grupo
+uso : just send the command
+note : Este comando pode ser usado se vocÃª for um administrador de grupo\n
+comando : *${prefix}simih*
+desc : activate simi mode in the group
+uso : *${prefix}simih 1* to activate simi mode and *${prefix}simih 0* to deactivate simi mode
+note : Este comando pode ser usado se vocÃª for um administrador de grupo\n`
+}
+
+exports.help = help
